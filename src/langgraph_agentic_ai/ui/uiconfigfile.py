1f4dc944a96514f0d4fe5a678b955678593f2e52
Soura <souradeep.ghosh.13@gmail.com>
2026-06-07 17:21:22 +0530

    Add UI configuration information

---
+++ src/langgraph_agentic_ai/ui/uiconfigfile.py
@@ -0,0 +1,20 @@
+from configparser import ConfigParser
+
+class config:
+    def __init__(self, config_file="./src/langgraph_agentic_ai/ui/uiconfigfile.ini"):
+        self.config=ConfigParser()
+        sef.config.read(config_file)
+        
+    def get_page_title(self):
+        return self.config.get("DEFAULT", "PAGE_TITLE")      
+    
+    def get_llm_options(self):
+        return self.config.get("DEFAULT", "LLM_OPTIONS").split(", ")
+    
+    def get_usecase_options(self):
+        return self.config.get("DEFAULT", "USECASE_OPTIONS").split(", ")
+    
+    def get_groq_model_options(self):
+        return self.config.get("DEFAULT", "GROQ_MODEL_OPTIONS").split(", ")
+    
+    
